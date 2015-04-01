68ab9756c4a69726a311b036ece689c0a8d9a0b3
MichealKenny <KennyMataz@gmail.com>
2015-04-01 07:26:42 +0100

    Added token utility for generating Reddit refresh tokens.

---
+++ token_util.py
@@ -0,0 +1,34 @@
+from bottle import route, request, run
+from praw import Reddit
+from json import loads
+import webbrowser
+
+#Load configuration file.
+try:
+    config_file = open('config.json', 'r')
+    config = loads(config_file.read())
+    config_file.close()
+
+except FileNotFoundError:
+    print('[Error]: config.json not found, make sure to edit and rename sample_config.json')
+    exit()
+
+settings = config['settings']
+version = settings['version']
+user_agent = settings['user_agent'].format(version=version)
+
+#Load instance of Reddit.
+reddit = Reddit(user_agent=user_agent)
+reddit.set_oauth_app_info(client_id=settings['client_id'], client_secret=settings['client_secret'],
+                          redirect_uri=settings['redirect_uri'])
+
+url = reddit.get_authorize_url('uniqueKey', 'identity,submit,read', True)
+webbrowser.open_new_tab(url)
+
+@route('/authorize_callback')
+def main():
+    code = request.query.get('code', '')
+    access_information = reddit.get_access_information(code)
+    return('refresh token: ' + access_information['refresh_token'])
+
+run(host='localhost', port=65010)
